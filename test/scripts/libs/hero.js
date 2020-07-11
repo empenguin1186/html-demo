53d35377045787178d586eff5396aefbe9637c87
empenguin1186 <emperorpenguint1186@gmail.com>
2020-07-12 00:39:16 +0900

    :+1: hero スライダー実装

---
+++ test/scripts/libs/hero.js
@@ -0,0 +1,16 @@
+const mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+
+    autoplay: {
+        delay: 5000,
+        // disableOnInteraction: true
+    },
+    fadeEffect: {
+        crossFade: true
+    },
+    // coverflowEffect: {
+    //     rotate: 30,
+    //     slideShadows: false,
+    // },
+});
